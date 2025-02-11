73f927f1d750917f2f33b4d8e334c96c0bf56041
Danivan0 <ivanov.daniel1987@gmail.com>
2025-02-11 18:54:36 +0200

    2/11/2025 .v2

--- 01. Integer Operations/Program.cs
+++ 01. Integer Operations/Program.cs
@@ -24,6 +24,7 @@ int num2 = int.Parse(Console.ReadLine());
 int num3 = int.Parse(Console.ReadLine());
 int num4 = int.Parse(Console.ReadLine());
 int num5 = int.Parse(Console.ReadLine());
+int num6 = int.Parse(Console.ReadLine()); 
 
 int first = num1 + num2;
 int second = first / num3;
